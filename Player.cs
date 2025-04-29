8cc829762d9b5175176f82a35a62b68607138cd0
samyaza5 <samyaza5@naver.com>
2025-04-29 22:45:39 +0900

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    Animation animator;
+    Animator animator;
     Rigidbody2D _rigidbody;
     Collider2D collider2d;
 
@@ -15,18 +15,75 @@ public class NewMonoBehaviourScript : MonoBehaviour
     public float gadMod = 6f;
     void Start()
     {
-        GetComponent<Rigidbody2D>();
-        GetComponent<Collider2D>();
-        GetComponent<Animation>();
         _rigidbody = GetComponent<Rigidbody2D>();
-        collider2d = GetComponent<Collider2D>();
-        animator = GetComponent<Animation>();
+        animator = GetComponentInChildren<Animator>();
 
-        
+       if(animator == null)
+        {
+            Debug.LogError("Animator not found in children.");
+        }
+       if (_rigidbody == null)
+        {
+            Debug.LogError("Rigidbody2D not found.");
+        }
     }
 
     void Update()
     {
-        
+        if (isDead)
+        {
+      
+            if (deathCooldown <= 0f)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                deathCooldown -= Time.deltaTime;
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonUp(0))
+            {
+                isFlap = true;
+            }
+
+        }
     }
+
+    private void FixedUpdate()
+    {
+        if (isDead) return;
+        {
+            Vector3 velocity = _rigidbody.linearVelocity;
+            velocity.x = forwardForce;
+
+            if (isFlap)
+            {
+                velocity.x = forwardForce;
+                velocity.y += speed;
+                isFlap = false;
+            }
+
+
+            _rigidbody.linearVelocity = velocity;
+
+            float angle = Mathf.Clamp((velocity.y * 10f), -90, 90);
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+
+        } 
+    }
+        
+        private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (isDead) return;
+        isDead = true;
+        deathCooldown = 1f;
+
+        animator.SetInteger("IsDie",1);
+
+    }
+
 }
+
